b296f2214c7a7ae1c155589b149affa6495c9766
Samuel Warner <sam_warner@talktalk.net>
2018-03-17 11:45:19 +0100

    added events to schema

--- tabledef.py
+++ tabledef.py
@@ -3,24 +3,44 @@ from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy import Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
- 
+
 engine = create_engine('sqlite:///meetme.db', echo=True)
 Base = declarative_base()
- 
+
 ########################################################################
 class User(Base):
     """"""
     __tablename__ = "users"
- 
+
     id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
- 
+
     #----------------------------------------------------------------------
     def __init__(self, username, password):
         """"""
         self.username = username
         self.password = password
- 
+
+########################################################################
+class Event(Base):
+    """"""
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True)
+    created_by = Column(String)
+    event_name = Column(String)
+    event_description = Column(String)
+    event_location = Column(String)
+    active = Column(Boolean)
+
+    #----------------------------------------------------------------------
+    def __init__(self, created_by, event_name, event_description, event_location, active):
+        """"""
+        self.created_by = created_by
+        self.event_name = event_name
+        self.event_description = event_description
+        self.event_location = event_location
+        self.active = active
 # create tables
 Base.metadata.create_all(engine)
